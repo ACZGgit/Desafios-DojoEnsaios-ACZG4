119ab9a3ca2e28bd866f13b1faf4d5eef9e8e0ef
raonieqr <raoniestevan@gmail.com>
2023-10-11 20:03:58 -0300

    feature: calcularPeso

---
+++ src/main/groovy/ensaio2ListaDeExerciciosPOO/exercicio7/services/PedidoService.groovy
@@ -0,0 +1,9 @@
+package ensaio2ListaDeExerciciosPOO.exercicio7.services
+
+import ensaio2ListaDeExerciciosPOO.exercicio7.entities.Pedido
+
+class PedidoService {
+  public static double calcularPeso(Pedido pedido) {
+    double pesoTotal = pedido.getAlimentos()
+  }
+}
