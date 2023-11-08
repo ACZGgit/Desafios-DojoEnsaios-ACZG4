f991fa78adf347d01a889464704869fb491fe13a
Sophia Luna <sophialuna3264@gmail.com>
2023-11-08 19:24:18 -0300

    FEAT processarTempoDeEntregaPorEntregador()

--- src/test/groovy/ensaio2ListaDeExerciciosPOO/exercicio7/services/EntregaServiceTest.groovy
+++ src/test/groovy/ensaio2ListaDeExerciciosPOO/exercicio7/services/EntregaServiceTest.groovy
@@ -16,15 +16,20 @@ class EntregaServiceTest extends GroovyTestCase {
 
     static Endereco enderecoRestaurante
     static Endereco posicaoEntregador
+    static Endereco posicaoEntregador2
+    static Endereco posicaoEntregador3
     static Endereco enderecoEntrega
     static Restaurante restaurante
     static Entregador entregador
+    static Entregador entregador2
+    static Entregador entregador3
     static EntregaService entregaService
     static Pedido pedido
     static List<Alimento> alimentos
 
     @BeforeAll
-    static void instanciaIdentificadorDePalavrasService() {
+    static void instanciaCenario() {
+
         alimentos = [new Alimento("Alimento A", 5, 3),
                      new Alimento("Alimento B", 5, 3),
                      new Alimento("Alimento C", 5, 6)
@@ -32,18 +37,30 @@ class EntregaServiceTest extends GroovyTestCase {
 
         enderecoRestaurante = new Endereco(10, 20)
         posicaoEntregador = new Endereco(30, 40)
+        posicaoEntregador2 = new Endereco(20, 20)
+        posicaoEntregador3 = new Endereco(15, 30)
         enderecoEntrega = new Endereco(50, 60)
+
         restaurante = new Restaurante("Restaurante A", enderecoRestaurante, [])
         pedido = new Pedido(alimentos, 15 as BigDecimal, enderecoEntrega)
+
         entregador = new Entregador("Entregador A",
           new Veiculo("Carro A", 70), 50,
             30, [], posicaoEntregador, 10 as BigDecimal, 10 as BigDecimal)
+        entregador2 = new Entregador("Entregador B",
+                new Veiculo("Carro B", 60), 40,
+                25, [], posicaoEntregador2, 12 as BigDecimal, 12 as BigDecimal)
+        entregador3 = new Entregador("Entregador C",
+                new Veiculo("Carro C", 80), 60,
+                35, [], posicaoEntregador3, 11 as BigDecimal, 11 as BigDecimal)
+
+
         entregaService = new EntregaService()
 
     }
 
     @Test
-    void calculoDeDistanciaTest() {
+    void calculoDeDistanciaRestauranteEntregadorTest() {
         //given:
         double distanciaEsperada = 28.28
 
@@ -95,5 +112,16 @@ class EntregaServiceTest extends GroovyTestCase {
 
         //then:
         assertEquals(tempoDeEntregaEsperado, tempoDeEntrega)
+    }
+
+    @Test
+    void processarTempoDeEntregaPorEntregadorTest() {
+        //Na frente de cada tempo, consta o preço do frete, calculado pelo peso dos pedidos e km rodados na entrega;
+        //given:
+        String saidaEsperada = "Entregador: Entregador B, Tempo: 1.331, Preco: " +
+                ""
+        //when:
+        pr
+
     }
 }
